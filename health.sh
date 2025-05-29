314749d73ff83903bda63bbbbd3dd21cb50b83c1
Brendan Dileo <124395674+brendanddev@users.noreply.github.com>
2025-05-29 17:56:39 -0400

    Update health.sh

--- health.sh
+++ health.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "Running System Health Check"
+echo "Running System Health Check..."
 
 echo -e "CPU Load:"
 uptime
 
 echo -e "Memory Usage:"
+free -h
+
 echo -e "Disk Usage:"
+df -h
+
 echo -e "Network Info:"
 echo -e "Uptime:"
