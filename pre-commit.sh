845fac56e5396bb23fd1864cd1e21ec38e99cfa5
Brendan Dileo <124395674+brendanddev@users.noreply.github.com>
2025-06-02 15:23:23 -0400

    Update pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+# Basic script to check which branch the commit is made to
+
+# $(...) --> Command runs inside the parentheses, and substitues its output in place
+
+BRANCH_NAME=$(git symbolic-ref --short HEAD)              # Uses the git command to get the current branch name, and stores in shell var
+
+
+
+
+
+
