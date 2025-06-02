41ea703569864d7fb650b12ff6bb7b29310535ef
Brendan Dileo <124395674+brendanddev@users.noreply.github.com>
2025-06-02 16:12:04 -0400

    Update check-commit-msg.sh

--- hooks/check-commit-msg.sh
+++ hooks/check-commit-msg.sh
@@ -9,5 +9,28 @@
 # newrev: The SHA of the new commit being pushed or updated
 # If newrev is all 0's (000000...), a branch is being deleted
 
+# Refs (References): Pointers to commits like refs/heads/main or refs/tags/v1.0
+
+# Refname: A full name of the ref being updated
+
+# Head (HEAD): Points to the current branch reference
+
 # -----------------------------------------------------------------------------------------------------------------
 # A custom hook to validate the format of a commit message.
+
+set -e                                                                      # Exits script immediately if any command fails
+
+COMMIT_MSG_FILE=$1                                                          # Commit message file is first arg passed to the script
+
+COMMIT_MSG=$(cat "$COMMIT_MSG_FILE")                                        # Read commit message, store into a var
+
+REGEX="^[A-Z]{2,5}-[0-9]+: .+"                                              # Defines regex to force prefix like "ABC-123" at start of msg
+
+if ! echo "$COMMIT_MSG" | grep -Eq "$REGEX"; then
+  echo "Commit Message is invalid!"
+  echo "Expected Format: ABC-123: Short message"
+  exit 1
+fi
+
+echo "Commit Message Format is valid!"
+exit 0
