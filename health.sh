e9729a3d987474aedee1dc2d3341cd9d62b2a20f
brendanmohawk <brendan.dileo@mohawkcollege.ca>
2025-06-02 15:00:25 -0400

    Update health.sh

--- health.sh
+++ health.sh
@@ -1,15 +1,1 @@
 #!/bin/bash
-
-echo "Running System Health Check..."
-
-echo -e "CPU Load:"
-uptime
-
-echo -e "Memory Usage:"
-free -h
-
-echo -e "Disk Usage:"
-df -h
-
-echo -e "Network Info:"
-echo -e "Uptime:"
