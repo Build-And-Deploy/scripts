b06fa448b72ab20639435df2ce25d65d034b5837
Brendan Dileo <124395674+brendanddev@users.noreply.github.com>
2025-06-02 16:04:59 -0400

    Notes 1

--- hooks/check-commit-msg.sh
+++ hooks/check-commit-msg.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# A custom hook to validate the format of a commit message.
+# SHA: Secure Hash Algorithm --> A unique id used by git to represent the state of the repository at any given point
+# It is a unique fingerprint for a git object (ussually a commit), in a 40 char hexadecimal string
 
+# oldrev: The SHA of the current/previous commit before the push
+# If oldrev is all 0's (000000...), a new branch is being created
+
+# newrev: The SHA of the new commit being pushed or updated
+# If newrev is all 0's (000000...), a branch is being deleted
+
+# -----------------------------------------------------------------------------------------------------------------
+# A custom hook to validate the format of a commit message.
